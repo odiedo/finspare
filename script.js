f165f0b1c35c2c85ce1168d8b3e26bffd26d75ef
odiedo <odiedopaul@gmail.com>
2024-07-17 18:34:05 +0300

    items insertions

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 // script.js
 $(document).ready(function() {
-    $('#requestBtn').click(function() {
-        $('#page1').hide();
-        $('#page2').show();
-    });
 
     $('#addItemBtn').click(function() {
-        $('.items').append(`
-            <div class="item">
+        $('.item-field-details').append(`
+            <div class="item-fields">
                 <input type="text" placeholder="Item name">
                 <input type="number" placeholder="Qnty">
-                <button class="remove-item">➖</button>
+                <button class="remove-item border-0 bg-transparent"><i class="fas fa-times"></i></button>
             </div>
         `);
     });
 
     $(document).on('click', '.remove-item', function() {
-        $(this).closest('.item').remove();
+        $(this).closest('.item-fields').remove();
     });
 });
