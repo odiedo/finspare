97bcd810c9ec8787cf7366f422faf2cfc689bf80
odiedo <odiedopaul@gmail.com>
2024-07-22 08:49:09 +0300

    spare rquest mail

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -1,25 +1,10 @@
 const nodemailer = require('nodemailer');
 
 exports.handler = async (event, context) => {
-
-  if (event.httpMethod === 'OPTIONS') {
-    return {
-      statusCode: 204,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type'
-      }
-    };
-  }
-  
   if (event.httpMethod !== 'POST') {
     return {
       statusCode: 405,
-      body: JSON.stringify({ message: 'Method Not Allowed' }),
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
+      body: JSON.stringify({ message: 'Method Not Allowed' })
     };
   }
 
@@ -83,18 +68,12 @@ exports.handler = async (event, context) => {
     await transporter.sendMail(mailOptions);
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Email sent successfully' }),
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
+      body: JSON.stringify({ message: 'Email sent successfully' })
     };
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Failed to send email', error }),
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
+      body: JSON.stringify({ message: 'Failed to send email', error })
     };
   }
 };
